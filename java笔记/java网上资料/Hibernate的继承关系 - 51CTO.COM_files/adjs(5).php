7f8427a826584c35b5ffc8af7c5fcb89fd04b01d
wuliang <1181169734@qq.com>
2013-10-04 19:06:26 +0800

    增加并修改了笔记

--- java笔记/java网上资料/Hibernate的继承关系 - 51CTO.COM_files/adjs(5).php
+++
@@ -1,5 +0,0 @@
-var phpadsbanner = '';
-
-phpadsbanner += '<'+'a href=\'http://vd.51cto.com/adclick.php?bannerid=8714&amp;zoneid=455&amp;source=&amp;dest=http%3A%2F%2Fmobile.51cto.com%2Fexp%2Fsalon%2Faction.php\' target=\'_blank\'><'+'img src=\'http://vd.51cto.com/adimage.php?filename=300_266.jpg&amp;contenttype=jpeg\' width=\'300\' height=\'260\' alt=\'\' title=\'\' border=\'0\'><'+'/a><'+'div id="beacon_8714" style="position: absolute; left: 0px; top: 0px; visibility: hidden;"><'+'img src=\'http://vd.51cto.com/adlog.php?bannerid=8714&amp;clientid=8299&amp;zoneid=455&amp;source=&amp;block=0&amp;capping=0&amp;cb=c49b91fc9ba8b1d603b85b3237f01ed2\' width=\'0\' height=\'0\' alt=\'\' style=\'width: 0px; height: 0px;\'><'+'/div>';
-
-document.write(phpadsbanner);
--- java笔记/java网上资料/Hibernate的继承关系 - 51CTO.COM_files/artcomment1105.php
+++
@@ -1,2 +0,0 @@
-document.write("<li><div class='commhd'><div class='fl'>lifvc</div><div class='fr commdate'>10-22 11:23</div></div><div class='blank1'></div><div class='commcon'>在第一个例子中，在一个表中，如何指定的personType这个属性呀？</div></li>");
- 
